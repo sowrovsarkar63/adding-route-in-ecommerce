56aa2426507713015d610b207d82a7e4a56ec7b6
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-03 03:39:58 +0600

    basic routing setup done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import Notfound from "./components/NotFound/Notfound";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Shop></Shop>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <Shop></Shop>
+                    </Route>
+                    <Route path="/shop">
+                        <Shop></Shop>
+                    </Route>
+
+                    <Route path="/review">
+                        <OrderReview></OrderReview>
+                    </Route>
+                    <Route path="/inventory">
+                        <Inventory></Inventory>
+                    </Route>
+                    <Route path="*">
+                        <Notfound></Notfound>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
