77e31101a51e3a7285c44b188a0c83210853e6ff
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-03 18:24:19 +0600

    Requirement completed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import Shop from "./components/Shop/Shop";
 function App() {
     return (
         <div>
-            <Header></Header>
             <BrowserRouter>
+                <Header></Header>
+
                 <Switch>
                     <Route exact path="/">
                         <Shop></Shop>
